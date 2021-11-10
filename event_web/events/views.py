145b81cb8d0ca495098056124af9ff0698e70e1f
stevescilar <stevemuambi@gmail.com>
2021-11-10 15:55:26 +0300

    added calender

--- event_web/events/views.py
+++ event_web/events/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+def home (request, year , month):
+    name =  "Stephen"
+    month = month.title()
+    #convert month from name to number
+    month_number =  list(calendar.month_name).index(month)
+    month_number = int(month_number) #ensure that calendar number is actually a number
 
-def home (request):
-    return render(request,'home.html',{})
+    #creating a calender object
+    cal = HTMLCalendar().formatmonth(year,month_number)
+    return render(request,'home.html',{
+        "name": name,
+        "year": year,
+        "month": month,
+        "month_number":month_number,
+        "cal": cal
+    })
